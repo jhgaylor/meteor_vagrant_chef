cf667679d0e7797c01f1e0e012c47684a6b2f20a
Jake Gaylor <jhgaylor@gmail.com>
2014-03-05 22:50:31 -0600

    add metadata

--- cookbooks/leaderboard/metadata.rb
+++ cookbooks/leaderboard/metadata.rb
@@ -5,3 +5,12 @@ license             'MIT'
 description         'Configures a meteor server'
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version             '0.0.1'
+
+supports "ubuntu"
+
+depends "apt"
+depends "nginx"
+depends "meteor"
+depends "sudo"
+
+recipe  "leaderboard", "Installs the whole app stack"
