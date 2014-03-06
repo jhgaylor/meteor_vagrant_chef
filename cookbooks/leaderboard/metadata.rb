52049c2fcaa1f91c998b3f329465e66b0ddf2707
Jake Gaylor <jhgaylor@gmail.com>
2014-03-05 19:47:52 -0600

    added metadata content
    Here we use the metadata.rb file to tell chef and other developers a
    bit about our cookbook

---
+++ cookbooks/leaderboard/metadata.rb
@@ -0,0 +1,7 @@
+name                'leaderboard'
+maintainer          '<you>'
+maintainer_email    '<you@example.com>'
+license             'MIT'
+description         'Configures a meteor server'
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version             '0.0.1'
